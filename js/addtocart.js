f886f89006d9b473d44dbc88a6701ea07e161cee
krookroo <robs16manalo@gmail.com>
2016-12-12 01:50:27 +0800

    modified shop.html(added modal for Add Cart), addtocart.js, new modal.css

--- js/addtocart.js
+++ js/addtocart.js
@@ -5,9 +5,37 @@ var itemAmount = 0;
 var productCount = 0;
 
 function addToCart(itemAmount, productCount) {
-	totalAmount += itemAmount;
+	totalAmount += (itemAmount * productCount);
 	totalItems += productCount;
 	
 	$("#cart-amount")[0].innerHTML = "$" + totalAmount;
+	sessionStorage.setItem("totalAmount", "$" + totalAmount);
 	$("#product-count")[0].innerHTML = totalItems;
+	sessionStorage.setItem("totalItems", totalItems);
+	
+	//should prevent refreshing page on form submit
+	//code here
 }
+
+$(document).ready(function() {
+	//temp solution
+	//store values on sessionStorage, does not work on IE
+	$("#cart-amount")[0].innerHTML = sessionStorage.getItem("totalAmount"); 
+	$("#product-count")[0].innerHTML = sessionStorage.getItem("totalItems");
+}); 
+
+var modal;
+function openModal() {
+	modal = $("#add-to-cart-modal");
+	modal.css("display", "block");
+}
+
+function closeModal() {
+	modal.css("display", "none");
+}
+
+//window.onclick = function(event) {
+//    if (event.target == modal) {
+//        modal.css("display", "none");
+//    }
+//}
